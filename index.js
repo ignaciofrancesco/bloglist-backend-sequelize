a0bdd203918691712c01b6fa5267bf37b8f0a079
ignacio <ignaciofranciscovigo@gmail.com>
2025-03-04 23:31:21 -0300

    Ex 13.3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { Sequelize } = require("sequelize");
+const { Sequelize, QueryTypes } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -11,10 +11,23 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
+const printBlogs = async () => {
+  const blogs = await sequelize.query("SELECT * FROM blogs", {
+    type: QueryTypes.SELECT,
+  });
+
+  blogs.forEach((b) => {
+    console.log(`${b.author}: '${b.title}' ${b.likes} likes.`);
+  });
+};
+
 const main = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
+
+    await printBlogs();
+
     sequelize.close();
   } catch (error) {
     console.error("Unable to connect to the database:", error);
@@ -22,3 +35,11 @@ const main = async () => {
 };
 
 main();
+
+/* Exercise 13.3.
+Create a functionality in your application which prints the blogs in the database on the command-line, e.g. as follows:
+
+$ node cli.js
+Executing (default): SELECT * FROM blogs
+Dan Abramov: 'On let vs const', 0 likes
+Laurenz Albe: 'Gaps in sequences in PostgreSQL', 0 likes */
