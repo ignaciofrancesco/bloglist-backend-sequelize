e1261fb81156d548bcd6039bb34bc1b412b5c035
ignacio <ignaciofranciscovigo@gmail.com>
2025-03-05 19:57:38 -0300

    Ex 13.4

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+const { Sequelize, QueryTypes, Model, DataTypes } = require("sequelize");
 
-const { Sequelize, QueryTypes } = require("sequelize");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -11,35 +15,91 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-const printBlogs = async () => {
-  const blogs = await sequelize.query("SELECT * FROM blogs", {
-    type: QueryTypes.SELECT,
-  });
+class Blog extends Model {}
 
-  blogs.forEach((b) => {
-    console.log(`${b.author}: '${b.title}' ${b.likes} likes.`);
-  });
-};
-
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-
-    await printBlogs();
-
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
   }
+);
+
+const syncBlog = async () => {
+  await Blog.sync();
 };
 
-main();
+syncBlog();
 
-/* Exercise 13.3.
-Create a functionality in your application which prints the blogs in the database on the command-line, e.g. as follows:
+/* ROUTES */
 
-$ node cli.js
-Executing (default): SELECT * FROM blogs
-Dan Abramov: 'On let vs const', 0 likes
-Laurenz Albe: 'Gaps in sequences in PostgreSQL', 0 likes */
+app.get("/api/blogs", async (req, res) => {
+  try {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = req.body;
+    console.log(blog);
+
+    const createdBlog = await Blog.create(blog);
+
+    console.log(createdBlog);
+
+    res.status(201).json(createdBlog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blogId = req.params.id;
+
+  try {
+    const result = await Blog.destroy({
+      where: {
+        id: blogId,
+      },
+    });
+
+    res.status(204).json({ result });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
+
+/* APP STARTUP */
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
