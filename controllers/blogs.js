3bb953ccc88c1b525625c94a30a07270a7abae77
ignacio <ignaciofranciscovigo@gmail.com>
2025-03-06 20:40:28 -0300

    Ex 13.7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,64 +3,52 @@ const Blog = require("../models/blog");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const blogs = await Blog.findAll();
-    res.json(blogs);
-  } catch (error) {
-    res.status(400).json({ error: error });
-  }
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+  res.status(400).json({ error: error });
 });
 
 router.post("/", async (req, res) => {
-  try {
-    const blog = req.body;
-    console.log(blog);
-
-    const createdBlog = await Blog.create(blog);
-
-    console.log(createdBlog);
-
-    res.status(201).json(createdBlog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
+  const blog = req.body;
+  const createdBlog = await Blog.create(blog);
+  res.status(201).json(createdBlog);
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", async (req, res, next) => {
   const blogId = req.params.id;
   const newLikes = req.body.likes;
 
-  try {
-    const blog = await Blog.findByPk(blogId);
-
-    if (!blog) {
-      res.status(404).end();
-    }
-
-    blog.likes = req.body.likes;
-
-    const updatedBlog = await blog.save();
-
-    res.status(200).json({ likes: newLikes });
-  } catch (error) {
-    res.status(400).json({ error: error });
+  if (!Number.isInteger(Number(blogId))) {
+    const error = new Error("Invalid ID format");
+    error.name = "BadRequestError";
+    return next(error); // Pass the error to the error handler
   }
+
+  const blog = await Blog.findByPk(blogId);
+
+  if (!blog) {
+    const notFoundError = new Error("Blog not found.");
+    notFoundError.name = "NotFoundError";
+    return next(notFoundError);
+  }
+
+  blog.likes = req.body.likes;
+
+  const updatedBlog = await blog.save();
+
+  res.status(200).json({ likes: newLikes });
 });
 
 router.delete("/:id", async (req, res) => {
   const blogId = req.params.id;
 
-  try {
-    const result = await Blog.destroy({
-      where: {
-        id: blogId,
-      },
-    });
+  const result = await Blog.destroy({
+    where: {
+      id: blogId,
+    },
+  });
 
-    res.status(204).json({ result });
-  } catch (error) {
-    res.status(400).json({ error: error });
-  }
+  res.status(204).json({ result });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { connectToDatabase } = require("./utils/db");
 const blogsRouter = require("./controllers/blogs");
 
 const express = require("express");
+require("express-async-errors");
 const app = express();
 
 /* MIDDLEWARE 1 */
@@ -13,6 +14,41 @@ app.use(express.json());
 /* ROUTES */
 
 app.use("/api/blogs", blogsRouter);
+
+/* MIDDLEWARE 2 */
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.stack);
+
+  // Sequelize errors
+  if (error.name === "SequelizeDatabaseError") {
+    return res.status(400).json({ error: "Malformed request" });
+  }
+
+  if (error.name === "SequelizeValidationError") {
+    return res
+      .status(400)
+      .json({ error: error.errors.map((e) => e.message).join(", ") });
+  }
+
+  if (error.name === "SequelizeUniqueConstraintError") {
+    return res.status(400).json({ error: "Duplicate entry detected" });
+  }
+
+  // Other common errors, that i have to create and next myself.
+  if (error.name === "BadRequestError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  if (error.name === "NotFoundError") {
+    return res.status(404).json({ error: error.message });
+  }
+
+  // Unknown errors
+  return res.status(500).json({ error: "Internal server error" });
+};
+
+app.use(errorHandler);
 
 /* APP STARTUP */
 
